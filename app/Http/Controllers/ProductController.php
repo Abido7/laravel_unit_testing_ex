192f0ee218bf6e4f97e4b87564e3502bbca875d9
abido7 <mahmoudabido90@gmail.com>
2023-06-04 19:40:14 +0300

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'products' => Product::get(),
+        ], Response::HTTP_OK);
+
+    }
+}
